481cc71839a8a69380751429b858d00254da64e8
vliubimtsev-rm <v.liubimtsev@rautenberg.media>
2019-01-15 02:05:25 +0200

    Add method evaluation calculation

--- src/main/java/com/maverick/domain/data/Sale.java
+++ src/main/java/com/maverick/domain/data/Sale.java
@@ -3,6 +3,10 @@ package com.maverick.domain.data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.util.Pair;
+
+import java.time.YearMonth;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -12,5 +16,33 @@ public class Sale {
     private int expectedQuantity;
     private int customCoefficientQuantity;
     private int movingAverageQuantity;
+
+    public static void main(String[] args) {
+
+
+        int couldSold = 0;
+        int redundantSold = 0;
+
+
+        Pair<Integer, Integer> evaluation = Pair.of(0, 0);
+        Map<YearMonth, Integer> map = Map.of(YearMonth.of(2017, 1), 1000, YearMonth.of(2017, 2), 1000, YearMonth.of(2017, 3), -2000);
+
+        for (Integer count : map.values()) {
+
+            if (count < 0) {
+                int compensated = count + redundantSold;
+                if (compensated > 0) {
+                    redundantSold += count;
+                } else {
+                    redundantSold = 0;
+                    couldSold += compensated;
+                }
+            } else {
+                redundantSold += count;
+            }
+        }
+        System.out.println("couldSold = " + couldSold);
+        System.out.println("redundantSold = " + redundantSold);
+    }
 
 }
